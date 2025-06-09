d1f4ea027d9600fa28b9b9f5551e59582656b99c
bbails21 <baileyortiz1999@gmail.com>
2025-06-09 04:31:11 +0000

    Lab 1-completed

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Output: greeting
+        System.out.println("Hello, World!");
+
+        // Output: personal answers
+        System.out.println("My name is Bailey.");
+        System.out.println("My favorite food is ramen.");
+        System.out.println("I would love to meet Hayley Williams.");
+        System.out.println("My least favorite chore is laundry.");
+        System.out.println("I’d rather be taking a nap right now.");
+    }
+}
